1955f19f57d3e79cd9db2e436717401df9b8c69c
CesarMMI <cesargustavo53@gmail.com>
2024-06-21 18:37:11 -0300

    front ent reset

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,16 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { httpErrorInterceptor } from './core/interceptors/http-error.interceptor';
-import { tokenInterceptor } from './core/interceptors/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(
-      withInterceptors([tokenInterceptor, httpErrorInterceptor])
-    ),
-    provideAnimationsAsync(),
-  ],
+  providers: [provideRouter(routes), provideAnimationsAsync()]
 };
